8f6fe660f9ced45f08779e459a0aed55f159b516
Adoria298 <bananaorange29@outlook.com>
2023-12-12 14:00:44 +0000

    Add conjj from Grenfell

--- csv-to-md.py
+++ csv-to-md.py
@@ -5,18 +5,24 @@ A Database of Greek and Latin Conjectural Emendations Attested in MSS
 
 <details>
     <summary>Bibliography</summary>
+    <h3>Papyri</h3>
     <ul>
-        <li>P.Oxy.I,II,LXXXI</li>
-        <li>James, Patrick. ‘Review of The Oxyrhynchus Papyri. Volume LXXXI.’ <em>The Classical Review</em> 68, no. 2 (2018): 395-398.</li>
-        <li>Kenyon, Frederic George. <em>The Evidence of Greek Papyri with Regard to Textual Criticism.</em> London: Hernry Frowde, 1904.</li>
-        <li>Wessely and Rzach, <em>Studien zur Palaeographie und Papyruskunde</em>, I (1901)</li>
-        <li>Weil, <em>Revue de Philologie</em>, xiii. 179 (1882)</li>
-        <li>Wilcken, <em>Sitzungsb. d. Berl. Akad.</em>, 1887 p. 807</li>
+        <li>P.Oxy.I,II,IV,V,VI,VII,VIII,X,XI,XIII,LXXXI</li>
         <li>P.Louvre ed. Weil = Austin 42</li>
         <li>P.Münch.II.40 = Pap.graec.mon.89</li>
-        <li>Papyrus Massiliensis</li>
+        <li>Papyrus Massiliensis</li>        
+        <li>P.Ryl.54</li>
+        <li><em>Berl. klasse Texte</em></li>
+        <li>P. Hibeh 24,26</li>
+    </ul>
+    <h3>Secondary Literature</h3>
+    <ul>
+        <li>James, Patrick. ‘Review of The Oxyrhynchus Papyri. Volume LXXXI.’ <em>The Classical Review</em> 68, no. 2 (2018): 395-398.</li>
+        <li>Kenyon, Frederic George. <em>The Evidence of Greek Papyri with Regard to Textual Criticism.</em> London: Hernry Frowde, 1904.</li>
+        <li>Grenfell, B. P. ‘The Value of Papyri for the Textual Criticism of Extant Greek Authors.’ <em>The Journal of Hellenic Studies</em> 39 (1919): 16-36.</li>
     </ul>
 </details>
+
 """
 
 with open("attested-conjectures.csv", "r", encoding="utf8") as conj_csv:
